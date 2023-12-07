e3445b1fbbe203808f31cc0bba79d323aaf917d7
Hasnathasan <hasnatoooooooo@gmail.com>
2023-12-07 22:05:25 +0600

    Bapp ar bank added to GitHub

---
+++ js/debug-steps.js
@@ -0,0 +1,26 @@
+/* 
+Debug Steps: 
+1. Error check (error reproduce)
+2. Check others stuffs on the website
+3. Check Console for error
+4. click on the link of the error (it will take you to the code)
+5. If needed add a breakpoint and try to stop the code over there and see the values
+6. if needed console log output
+7. search full code base (Ctrl + Shift + F) > don't forget about partial match
+8. look around for typo
+*/
+
+
+const checkNum = n => {
+    for(let i = 0; i <= 20; i++){
+        const result = Math.pow(3,i);
+        if(result == n){
+            console.log(true)
+            return
+        }
+    }
+    console.log(false);
+}
+
+checkNum(387420490)
+console.log(+"2");
